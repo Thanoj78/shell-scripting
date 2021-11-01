3cea469c2c3d6cfad116044a09c56c0afef4841f
Thanoj78 <thanojk12345@gmail.com>
2021-11-01 14:01:16 +0530

    pushing makefile and component scripts

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,4 +1,4 @@
-echo "Setting UP MongoDB Repo"
+echo "Installing MongoDB"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -7,20 +7,7 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
-echo "Installing MongoDB"
-yum install -y mongodb-org >/tmp/log
-
-echo "Configuring MongoDB"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
-echo "starting MongoDB"
+yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
-systemctl restart mongod
-echo "Dowloading MoangoDB Schema"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
-cd /tmp
-unzip mongodb.zip >/tmp/log
-cd mongodb-main
-mongo < catalogue.js >/tmp/log
-mongo <user.js >/tmp/log
