7e04e625c804022768eeaa003b925fab972b66dd
THANOJ78 <thanojk12345@gmail.com>
2021-11-02 20:32:21 +0530

    commiting

--- 08-functions.sh
+++ 08-functions.sh
@@ -3,14 +3,19 @@
 # Declare a function
 
 SAMPLE() {
+    a=100
     echo I am a function
     echo "Value of a in function = $a"
     b=20
+    echo "First Argument in Functions = $1"
 }
 
 ## Main Function
 
 ##  Access a function
 a=10
-SAMPLE
+SAMPLE xyz
+b=200
 echo "value of b in Main Program = $b"
+
+echo "First Argument in Main Program = $1"
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-echo "setting up Mongodb Repo"
+Status_Check() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mSUCCESS\e[0m"
+    exit 2
+  fi
+}
+
+Print() {
+  echo -n -e "$1 \t- "
+}
+
+Print "setting up Mongodb Repo"
 
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
@@ -8,71 +21,36 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
-echo "installing mongodb"
+Print "installing mongodb"
 yum install -y mongodb-org >/tmp/log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
-echo "Configuring MongoDB"
+Print "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
-echo"Starting MongoDB"
+Print "Starting MongoDB"
 systemctl enable mongod
 systemctl start mongod
 systemctl restart mongod
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
-echo "Downloading MongoDB Schema"
+Print "Downloading MongoDB Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
 cd /tmp
 echo "Extracting Schema Archive"
 unzip  -o mongodb.zip >/tmp/log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
 cd mongodb-main
 
 echo "Loading Schema"
 mongo < catalogue.js >/tmp/log
 mongo < users.js >/tmp?log
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mSUCCESS\e[0m"
-  exit 2
-fi
+Status_Check $?
 
 exit 0
