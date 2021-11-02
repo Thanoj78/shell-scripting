1c6484fabf6d44ba332a0608b6f9c55411cac768
THANOJ78 <thanojk12345@gmail.com>
2021-11-02 20:34:06 +0530

    commiting

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -23,15 +23,15 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 Status_Check $?
 
-Print "installing mongodb"
+Print "installing mongodb\t"
 yum install -y mongodb-org >/tmp/log
 Status_Check $?
 
-Print "Configuring MongoDB"
+Print "Configuring MongoDB\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 Status_Check $?
 
-Print "Starting MongoDB"
+Print "Starting MongoDB\t"
 systemctl enable mongod
 systemctl start mongod
 systemctl restart mongod
@@ -42,13 +42,13 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 Status_Check $?
 
 cd /tmp
-echo "Extracting Schema Archive"
+Print "Extracting Schema Archive"
 unzip  -o mongodb.zip >/tmp/log
 Status_Check $?
 
 cd mongodb-main
 
-echo "Loading Schema"
+Print "Loading Schema"
 mongo < catalogue.js >/tmp/log
 mongo < users.js >/tmp?log
 Status_Check $?
