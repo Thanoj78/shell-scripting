f0f98be487778874531937ddd176439fab7bd16f
THANOJ78 <thanojk12345@gmail.com>
2021-11-03 13:01:57 +0530

    commiting

--- roboshop/components/catalouge.sh
+++ roboshop/components/catalouge.sh
@@ -2,11 +2,11 @@
 
 source components/common.sh
 
-Print "Installing NodeJS\t"
+Print "Installing NodeJS\t\t"
 yum install nodejs make gcc-c++ -y &>>$LOG
 Status_Check $?
 
-Print "Adding Roboshop User\t"
+Print "Adding Roboshop User\t\t"
 id roboshop &>>$LOG
 if [ $? -eq 0 ]; then
   echo "User alread there, so skipping" &>>$LOG
@@ -15,11 +15,11 @@ else
 fi
 Status_Check $?
 
-Print "Downloading Catalouge Content"
+Print "Downloading Catalouge Content\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 Status_Check $?
 
-Print "Extracting Catalouge\t"
+Print "Extracting Catalouge\t\t"
 cd /home/roboshop
 rm -rf catalogue
 unzip /tmp/catalogue.zip &>>$LOG
