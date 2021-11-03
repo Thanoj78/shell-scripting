68399bd7a894d953cbbb4c6ae1237d9cde2544d8
THANOJ78 <thanojk12345@gmail.com>
2021-11-03 14:35:07 +0530

    commit catalouge

--- roboshop/components/catalouge.sh
+++ roboshop/components/catalouge.sh
@@ -1,16 +1,40 @@
-yum install nodejs make gcc-c++ -y
+#!/bin/bash
 
-useradd roboshop
+source components/common.sh
 
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Print "Installing NodeJS\t\t"
+yum install nodejs make gcc-c++ -y &>>$LOG
+Status_Check $?
 
+Print "Adding Roboshop User\t\t"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+  echo "User alread there, so skipping" &>>$LOG
+else
+  useradd roboshop &>>$LOG
+fi
+Status_Check $?
+
+Print "Downloading Catalouge Content\t"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+Status_Check $?
+
+Print "Extracting Catalouge\t\t"
 cd /home/roboshop
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
+rm -rf catalogue && unzip /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue
+Status_Check $?
 
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
+Print "Downloading NodeJS Dependencies"
+cd /home/roboshop/catalogue
+npm install --unsafe-perm &>>$LOG
+Status_Check $?
+
+chown roboshop:roboshop -R /home/roboshop
+
+Print "Setup SystemD Service\t\t"
+sed -i -e 's/MONGO DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalouge/systemd.service
+Status_Check $?
+
+Print "Setup SystemD Service"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
+Status_Check $?
