5ea3b57ac3a17bf6fa2b4248806ce74522614604
THANOJ78 <thanojk12345@gmail.com>
2021-11-03 14:21:11 +0530

    commiting

--- roboshop/components/catalouge.sh
+++ roboshop/components/catalouge.sh
@@ -1,15 +1,16 @@
 yum install nodejs make gcc-c++ -y
 
-useradd roboshop
+#useradd roboshop
 
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-cd /home/roboshop
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
+#curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl start catalogue
-systemctl enable catalogue
+#cd /home/roboshop
+#unzip /tmp/catalogue.zip
+#mv catalogue-main catalogue
+#cd /home/roboshop/catalogue
+#npm install
+
+#mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+#systemctl daemon-reload
+#systemctl start catalogue
+#systemctl enable catalogue
