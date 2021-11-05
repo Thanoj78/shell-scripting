4680934cbfd51ff2949ecdc0bfab9c22fabf2880
Thanoj78 <thanojk12345@gmail.com>
2021-11-05 17:30:16 +0530

    commit catalouge

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -5,18 +5,18 @@ Print "install Yum Utilities & Download Redies Repos"
 yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
 Status_Check $?
 
-Print "Setup Redies Repos"
+Print "Setup Redies Repos\t\t"
 yum-config-manager --enable remi &>>$LOG
 Status_Check $?
 
-Print "install Redies"
+Print "install Redies\t\t\t"
 yum install redis -y &>>$LOG
 Status_Check $?
 
-Print "Configure Redies Listen Address"
+Print "Configure Redies Listen Address\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 Status_Check $?
 
-Print "Start Redies Services"
+Print "Start Redies Services\t\t"
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 Status_Check $?
