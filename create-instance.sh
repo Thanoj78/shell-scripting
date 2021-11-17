cd28f73024de9db87b18d86b3d1776ab045cce86
Thanoj78 <thanojk12345@gmail.com>
2021-11-17 17:10:40 +0530

    commiting create instances

--- create-instance.sh
+++ create-instance.sh
@@ -9,5 +9,19 @@ if [ -z "${INSTANCE_NAME}" ]; then
   exit 1
 fi
 
-InstancesID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Instance $INSTANCE_NAME is already running"
+  exit 0
+fi
 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep stopped &>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Instance $INSTANCE_NAME is already created and stopped"
+  exit 0
+fi
+
+IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+
+sed -e "s/INSTANCE_NAME/$INSTANCE_NAME/" -e "s/INSTANCE_IP/$IP/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z00292693AN0C17DY2UW9 --change-batch file:///tmp/record.json | jq
