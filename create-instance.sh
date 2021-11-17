9e7ad763abd9c2871d37e2f194d56039a6fd0bf5
Thanoj78 <thanojk12345@gmail.com>
2021-11-17 13:38:08 +0530

    commit catalouge

--- create-instance.sh
+++ create-instance.sh
@@ -3,4 +3,5 @@
 LID=lt-0495b5dbb849fecb2
 LVER=2
 
-aws ec2 run-instances --launch-template LaunchTemplateID=$LID,Version=$LVER
+aws ec2 run-instances
+  --launch-template LaunchTemplateId=$LID,Version=$LVER
